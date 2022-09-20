4458a3ec29e0909aaf2208e9f94fa879ee96cabd
Kanat Atygayev <atygayev.kanat@gmail.com>
2022-09-20 17:44:50 +0600

    Добавлена настройка namespaces в urls

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 def todo_list(request):
+    todos = Todo.objects.all()
+    print(todos)
     return render(request, "todo/todo_list.html")
