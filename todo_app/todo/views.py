8a7e0a30e5d9132a573ed63e610056effed5359d
Kanat Atygayev <atygayev.kanat@gmail.com>
2022-09-20 17:46:36 +0600

    Добавлен context в views.py

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -1,8 +1,11 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Todo
 
 def todo_list(request):
     todos = Todo.objects.all()
-    print(todos)
-    return render(request, "todo/todo_list.html")
+    context = {
+        'todos': todos,
+    }
+    return render(request, "todo/todo_list.html", context)
