a17cf69721f8a30c411c5c5535f860c5c34e457a
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-21 10:06:25 +0600

    Добавлена детальная страница

--- todo_app/todo/views.py
+++ todo_app/todo/views.py
@@ -9,3 +9,12 @@ def todo_list(request):
         'todos': todos,
     }
     return render(request, "todo/todo_list.html", context)
+
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    context = {
+        'todo': todo,
+    }
+    return render(request, 'todo/todo_detail.html', context)
+
