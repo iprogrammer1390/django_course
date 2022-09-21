ecd9b7b07842130bad8caee152c88093f48c4fb7
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-21 21:16:54 +0600

    Добавлен методы к model

--- user_app/users/models.py
+++ user_app/users/models.py
@@ -1,15 +1,20 @@
 from tabnanny import verbose
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
 
 class User(models.Model):
     first_name = models.CharField(verbose_name="the persons first name", max_length=30)
     last_name = models.CharField(verbose_name="the persons last name", max_length=30)
     cars = models.ManyToManyField('Car', verbose_name="the user's cars")
 
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+    
+    
 
 STATUS_CHOICES = (
     ('R', 'Reviewed'),
@@ -24,6 +29,24 @@ class Website(models.Model):
     rating = models.IntegerField()
     status = models.CharField(choices=STATUS_CHOICES, max_length=1)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def was_relesed_last_week(self):
+        if self.release_date < datetime.date(2019, 3, 3):
+            return "Released before last week"
+        else:
+            return "Released this week"
+    
+    @property
+    def get_full_name(self):
+        return f"This is my website's full name: {self.name}"
+    
+    def get_absolute_url(self):
+        return f"/websites/{self.id}"
+    
+    def save(self, *args, **kwargs):
+        print("We are doing something else here...")
+        super().save(*args, **kwargs)
+    
 
     # class Meta:
     #     ordering = ['rating']
