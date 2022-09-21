ed44d1214956211a3b0e17b3f514d687b4fff626
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-21 15:48:52 +0600

    Создана модель Website, Добавлено поле status в модель website

--- user_app/users/models.py
+++ user_app/users/models.py
@@ -5,4 +5,24 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+STATUS_CHOICES = (
+    ('R', 'Reviewed'),
+    ('N', 'Not reviewed'),
+    ('E', 'Error'),
+    ('A', 'Accepted'),
+)
+class Website(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+    release_date = models.DateField()
+    rating = models.IntegerField()
+    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.rating}'
+
