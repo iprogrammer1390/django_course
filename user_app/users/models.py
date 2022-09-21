767c5656a44882b139079bac4f7c0f2f8243055b
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-09-21 16:00:13 +0600

    Добавлена модель Car и использовали verbose name

--- user_app/users/models.py
+++ user_app/users/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(verbose_name="the persons first name", max_length=30)
+    last_name = models.CharField(verbose_name="the persons last name", max_length=30)
+    cars = models.ManyToManyField('Car', verbose_name="the user's cars")
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -16,7 +17,7 @@ STATUS_CHOICES = (
     ('A', 'Accepted'),
 )
 class Website(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField( max_length=50)
     url = models.URLField()
     release_date = models.DateField()
     rating = models.IntegerField()
@@ -25,4 +26,7 @@ class Website(models.Model):
 
     def __str__(self):
         return f'{self.name} {self.rating}'
+
+class Car(models.Model):
+    name = models.CharField(max_length=40, primary_key=True)
 
